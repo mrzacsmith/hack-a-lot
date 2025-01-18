95dbbe2cb66a4760f175604175b32f88a67e326c
Zac Smith <zrs3141592@gmail.com>
2025-01-17 22:14:41 -0700

    👍 IMPROVE: styling for nav width

--- src/components/Dashboard/DashboardLayout.jsx
+++ src/components/Dashboard/DashboardLayout.jsx
@@ -3,15 +3,17 @@ import Sidebar from './Sidebar'
 
 const DashboardLayout = ({ setIsAuthenticated }) => {
   return (
-    <div className="flex h-[calc(100vh-64px)]">
-      {/* Sidebar */}
-      <Sidebar setIsAuthenticated={setIsAuthenticated} />
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex h-[calc(100vh-64px)]">
+        {/* Sidebar */}
+        <Sidebar setIsAuthenticated={setIsAuthenticated} />
 
-      {/* Main content */}
-      <div className="flex-1 bg-white">
-        <main className="h-full p-8">
-          <Outlet />
-        </main>
+        {/* Main content */}
+        <div className="flex-1 bg-white">
+          <main className="h-full p-8">
+            <Outlet />
+          </main>
+        </div>
       </div>
     </div>
   )
